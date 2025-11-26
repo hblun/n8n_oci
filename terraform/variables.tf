29daa31240b1ee4584bf7b2b332d803533ebaaac
hblun <47152348+hblun@users.noreply.github.com>
2025-11-26 11:31:25 +0000

    Add new variables for instance configuration
    Added variables for instance shape, OCPUs, memory, and availability domain.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,26 @@ variable "ssh_public_key" {
   type        = string
 }
 
+variable "instance_shape" {
+  description = "Shape of the compute instance"
+  type        = string
+  default     = "VM.Standard.A1.Flex"
+}
+
+variable "ocpus" {
+  description = "Number of OCPUs"
+  type        = number
+  default     = 1
+}
+
+variable "memory_in_gbs" {
+  description = "Memory in GB"
+  type        = number
+  default     = 6
+}
+
+variable "availability_domain" {
+  description = "Availability domain"
+  type        = string
+  default     = "btiH:UK-LONDON-1-AD-1"
+}
